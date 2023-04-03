c3015afb3838b3517c7c907bcb578daca88c8da7
Yurii Ivanyshchev <Megatomua@gmail.com>
2023-04-03 19:19:09 +0300

    changed package of class

--- src/main/java/careerhub/exception/GlobalExceptionHandler.java
+++ src/main/java/careerhub/exception/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
-package careerhub.controller;
+package careerhub.exception;
 
-import careerhub.exception.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
